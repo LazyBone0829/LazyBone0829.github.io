854ee38c8273c46e20e8104ec3a1265565d93ee4
lazybone0829 <lazybone0829@gmail.com>
2019-04-19 12:02:19 +0800

    Site updated: 2019-04-19 12:02:18

--- dist/music.js
+++ dist/music.js
@@ -1,6 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
+	lrcType: 3,
     autoplay: false,
     audio: [
       {
@@ -8,21 +9,21 @@ const ap = new APlayer({
         artist: 'Phoebe Ryan&The Chainsmokers',
         url: "/uploads/All We Know - The Chainsmokers,Phoebe Ryan.mp3",
         cover: '/uploads/all we know.jpg',
-		lrc: "/uploads/All We Know - The Chainsmokers,Phoebe Ryan.txt",
+		lrc: "/uploads/All We Know - The Chainsmokers,Phoebe Ryan.lrc",
       },
       {
         name: "T'en va pas",
         artist: 'Elsa',
         url: "/uploads/T'en va pas - Elsa.mp3",
         cover: "/uploads/T'en va pas.jpg",
-		lrc: "/uploads/T'en va pas - Elsa.txt",
+		lrc: "/uploads/T'en va pas - Elsa.lrc",
       },
       {
         name: "The Apl Song",
         artist: "The Black Eyed Peas",
         url: "/uploads/The Apl Song - The Black Eyed Peas.mp3",
         cover: "/uploads/The Apl Song.jpg",
-		lrc: "/uploads/The Apl Song - The Black Eyed Peas.txt",
+		lrc: "/uploads/The Apl Song - The Black Eyed Peas.lrc",
       }
     ]
 });
